807f9003c203194d1e1160d0f9b0699b5fed375c
dcorvera22 <diegocorvera@u.boisestate.edu>
2025-09-18 18:52:33 -0600

    Add files via upload

---
+++ Week 4 Assignment/Code/top.v
@@ -0,0 +1,38 @@
+module top(
+    input [3:0] sw,
+    output [2:0] led
+);
+
+//	naive nav ( 
+//		.A(sw[0]),
+//		.B(sw[1]),
+//		.C(sw[2]),
+//		.D(sw[3]),
+//		.Y(led[0])
+//		);
+	
+    naive naive_inst(
+        .A(sw[3]),
+        .B(sw[2]),
+        .C(sw[1]),
+        .D(sw[0]),
+        .Y(led[0])
+    );
+
+    minterm minterm_inst(
+        .A(sw[3]),
+        .B(sw[2]),
+        .C(sw[1]),
+        .D(sw[0]),
+        .Y(led[1])
+    );
+
+    maxterm maxterm_inst(
+        .A(sw[3]),
+        .B(sw[2]),
+        .C(sw[1]),
+        .D(sw[0]),
+        .Y(led[2])
+    );
+
+endmodule
